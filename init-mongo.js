7504d7b88eb61477786b60a863e927e2100ba627
Matt Ford <guru_fordy@hotmail.com>
2023-12-30 19:42:10 +0000

    Pulled DB init to new file

---
+++ init-mongo.js
@@ -0,0 +1,10 @@
+db.getSiblingDB("unifi").createUser({
+  user: "unifi",
+  pwd: "sharingiscaring223",
+  roles: [{ role: "readWrite", db: "unifi" }],
+});
+db.getSiblingDB("unifi_stat").createUser({
+  user: "unifi",
+  pwd: "sharingiscaring223",
+  roles: [{ role: "readWrite", db: "unifi_stat" }],
+});
